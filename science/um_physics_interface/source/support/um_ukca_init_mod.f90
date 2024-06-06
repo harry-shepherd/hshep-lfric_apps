d99d114a0a94d66967466736e0d80b6e984dd67e
jamesbruten <enquiries@metoffice.gov.uk>
2024-06-06 08:13:26 +0000

    ticket:154 : danielgrosvenor : Fix bugs in UKCA aerosol water content calculation (ukca_water_content_v.F90)
    git-svn-id: https://metomi/svn/lfric_apps.xm/main/trunk@1755 3896877f-8c05-4358-bc88-7947bb4308c7

--- science/um_physics_interface/source/support/um_ukca_init_mod.f90
+++ science/um_physics_interface/source/support/um_ukca_init_mod.f90
@@ -731,6 +731,7 @@ contains
            proc_bl_tracer_mix = bl_tracer_mix,                                 &
            ! UKCA temporary logicals
            l_fix_ukca_hygroscopicities=.false.,                                &
+           l_fix_ukca_water_content=.true.,                                    &
            ! Return status information
            error_message=ukca_errmsg,                                          &
            error_routine=ukca_errproc)
