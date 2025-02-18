7ebf72536694b97311af3ef94628117fe9959ecc
roddysharp <enquiries@metoffice.gov.uk>
2025-02-18 13:07:02 +0000

    ticket:559 : alisonmclauren : Various Improvements to Water Tracers
    git-svn-id: https://metomi/svn/lfric_apps.xm/main/trunk@7626 3896877f-8c05-4358-bc88-7947bb4308c7

--- applications/lfric_coupled/build/project.mk
+++ applications/lfric_coupled/build/project.mk
@@ -9,7 +9,8 @@ $(info Coupling project configuration)
 export PRE_PROCESS_INCLUDE_DIRS = \
         $(WORKING_DIR)/science/um/src/atmosphere/atmosphere_service/include \
         $(WORKING_DIR)/science/um/src/atmosphere/boundary_layer/include \
-        $(WORKING_DIR)/science/um/src/atmosphere/large_scale_precipitation/include
+        $(WORKING_DIR)/science/um/src/atmosphere/large_scale_precipitation/include \
+        $(WORKING_DIR)/science/um/src/atmosphere/free_tracers/include
 
 export PRE_PROCESS_MACROS += UM_PHYSICS LFRIC USSPPREC_32B LSPREC_32B UM_JULES
 
